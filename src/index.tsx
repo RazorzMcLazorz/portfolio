b59d11627ba5f3ee57affa590be12a1aab6566d3
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-05-25 23:08:22 -0600

    react 18 now working

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-// @ts-ignore
-import * as ReactDOMClient from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './index.scss'
 import App from './App'
@@ -8,20 +7,25 @@ import reportWebVitals from './reportWebVitals'
 
 const container = document.getElementById('root')
 
-// Create a root.
-const root = ReactDOMClient.createRoot(container)
+const root = createRoot(container!)
 
-// Initial render: Render an element to the root.
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
 
+// Initial render: Render an element to the root.
+// ReactDOM.render(
+//
+//     <App />
+//  ,
+//   document.getElementById('root')
+// )
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// @ts-ignore
 serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
