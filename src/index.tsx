4b0d90c58e0c85a256108208e4c46fef69ac6bd5
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-05-25 23:20:34 -0600

    svg icon is now working

--- src/index.tsx
+++ src/index.tsx
@@ -15,14 +15,6 @@ root.render(
   </React.StrictMode>
 )
 
-// Initial render: Render an element to the root.
-// ReactDOM.render(
-//
-//     <App />
-//  ,
-//   document.getElementById('root')
-// )
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
