77a9709764964dee8efcfafef226127a88f57936
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-05-26 00:11:51 -0600

    Contact Page is now fixed and completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import routes from './constants/routes'
 import Home from './components/home/Home'
 import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills'
+import Contact from './components/contact/Contact'
 
 function App() {
   return (
@@ -12,12 +13,14 @@ function App() {
           <Route path={routes.home} element={<Home />} />
           <Route path={routes.projects} element={<Projects />} />
           <Route path={routes.skills} element={<Skills />} />
+          <Route path={routes.contact} element={<Contact />} />
         </Routes>
       ) : (
         <Routes>
           <Route path={routes.home} element={<Home />} />
           <Route path={routes.projects} element={<Projects />} />
           <Route path={routes.skills} element={<Skills />} />
+          <Route path={routes.contact} element={<Contact />} />
         </Routes>
       )}
     </BrowserRouter>
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,6 +2,7 @@ class routes {
   static home = '/'
   static projects = '/projects'
   static skills = '/skills'
+  static contact = '/contact'
 }
 
 export default routes
